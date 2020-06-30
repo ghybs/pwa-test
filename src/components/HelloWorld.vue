043958b78b1e0aea5389d258456cc9d9c0affe95
ghybs <ghybs1@gmail.com>
2020-06-30 23:08:41 +0400

    feat(app): add html2pdf, to email, basic form

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,111 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Demo</h3>
+
+    <p>Some form:</p>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li>
+        Item free text:
+        <input type="text" v-model="item.text" placeholder="Free text..." />
+      </li>
+      <li>Item checkbox: <input type="checkbox" v-model="item.checked" /></li>
+      <li>
+        Item dropdown:
+        <select v-model="item.select">
+          <option>Item A</option>
+          <option>Value B</option>
+          <option>42</option>
+        </select>
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <button @click="toPdf">As PDF</button>
+
+    <br /><br />
+
+    <button @click="toEmail">To email</button> (but still need to manually
+    attach the PDF file to the email)
+    <br />
+    (this is a limitation of websites and PWA's, but not for Mobile Apps)
+
+    <hr />
+
+    <label :for="myId">Add a photo</label>
+    <input
+      :id="myId"
+      ref="input"
+      type="file"
+      accept="image/*"
+      :capture="autoCamera ? 'camera' : null"
+      @change="readURL"
+    />
+    <br />
+    <input :id="anotherId" type="checkbox" v-model="autoCamera" />
+    <label :for="anotherId"
+      >Automatically use camera to take a picture (otherwise open file
+      picker)</label
+    >
+
+    <div ref="forEmail">
+      <img class="img" :src="imgData" alt="your image" />
+    </div>
   </div>
 </template>
 
 <script>
+import html2pdf from "html2pdf.js";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      imgData: "",
+      item: {
+        text: undefined,
+        checked: false,
+        select: undefined,
+      },
+      myId: "someRandomUniqueIdentifier",
+      anotherId: "someOtherUniqueIdentifier",
+      autoCamera: true,
+    };
+  },
+  methods: {
+    readURL() {
+      const input = this.$refs.input;
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = (e) => {
+          this.imgData = e.target.result;
+        };
+
+        reader.readAsDataURL(input.files[0]);
+      }
+    },
+    toPdf() {
+      html2pdf()
+        .from(document.body)
+        .set({
+          filename: "hiy.pdf",
+        })
+        .save();
+    },
+    toEmail() {
+      //console.log(this.item);
+      let mailBody = `free text: ${this.item.text}%0D%0A<br>Checked? ${
+        this.item.checked ? "yes" : "no"
+      }%0D%0A<br>Dropdown selected value: ${this.item.select}`;
+      //mailBody = encodeURIComponent(mailBody);
+      window.location =
+        "mailto:yourmail@domain.com?subject=hii&body=" + mailBody;
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -44,15 +113,13 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
 li {
-  display: inline-block;
-  margin: 0 10px;
+  text-align: initial;
 }
 a {
   color: #42b983;
+}
+.img {
+  max-width: 100%;
 }
 </style>
